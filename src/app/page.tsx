185cb515b7a0cc1930800d8cfdbb5803281db37c
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-06-19 00:04:05 +0900

    feat(main): main page 디자인 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,58 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import S from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={S.main}>
+      <nav className={S.headerContainer}>
+        <div className={S.headerWrapper}>
+          <Link href={'/'}>
+            <div className={S.titleArea}>
+              <Image
+                src="/just-crud-logo.svg"
+                alt="Just CRUD"
+                width={40}
+                height={40}
+              />
+              <h1 className={S.title}>UserName</h1>
+            </div>
+          </Link>
+          <div className={S.iconArea}>
+            <Image src="/user.svg" alt="user-img" width={40} height={40} />
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </nav>
+      <section className={S.postListContainer}>
+        <div className={S.postListWrapper}>
+          <ul className={S.postList}>
+            <li>
+              <Link className={S.postItem} href={'/'}>
+                <div className={S.postItemWrapper}>
+                  <div className={S.postImgArea}>
+                    <Image
+                      src="https://doodleipsum.com/300x200/?bg=lightgray&n=4"
+                      alt="post-item"
+                      width={300}
+                      height={200}
+                      layout="responsive"
+                      objectFit="cover"
+                      style={{
+                        borderRadius: '12px',
+                      }}
+                    ></Image>
+                  </div>
+                  <div className={S.postInfoArea}>
+                    <p className={S.postCategori}>으헤헤</p>
+                    <h3 className={S.postTitle}>집에 정말정말 가고싶을때</h3>
+                    <p className={S.postDate}>2024.12.20</p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
